180275c3a3e43786c965eadcf748ef045446dd79
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2023-09-23 21:49:21 +0200

    Add assets

---
+++ Game.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update_ui()
+	pass
+
+
+
+
+func _on_check_button_toggled(button_pressed):
+	$State.toggle_engine(button_pressed)
+	pass # Replace with function body.
+
+
+func update_ui():
+	var state = $State as Gamestate
+	
+	$SpeedProgress.value = 100*(state.speed / state.speedTarget)
+	$SpeedProgress/Label.text = str(state.speed) + "m/s"
+	
+	$FuelProgress.value = 100*(state.fuel / state.tankSize)
+	
+	$EnginePower.button_pressed = state.engineRunning
---
+++ State.gd
@@ -0,0 +1,50 @@
+extends Node
+class_name Gamestate
+
+var fuel = 100.0
+var tankSize = 100.0
+var speed = 0.0
+var damage = 0.0
+var hull = 100.0
+
+var engineRunning = false
+var enginePower = 10
+
+var baseFuelConsumption = 0.7
+var shipAeroFactor = 0.9999
+
+var speedTarget = 300000000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+var deltaT = 0 
+func _process(delta):
+	deltaT += delta
+	if deltaT > 1:
+		deltaT -= 1
+		print_my_debug()
+
+	if fuel < 0:
+		fuel = 0
+		engineRunning = false
+	
+	if engineRunning:
+		fuel -= delta * baseFuelConsumption
+		speed += delta * enginePower
+
+	speed = max(0, speed * shipAeroFactor) #TODO factor in delta here	
+	
+	pass
+
+func toggle_engine(power):
+	engineRunning = power
+
+
+func print_my_debug():
+	var debug = "val: " + str(engineRunning) + "\t" + str(speed)
+
+	print(debug)
